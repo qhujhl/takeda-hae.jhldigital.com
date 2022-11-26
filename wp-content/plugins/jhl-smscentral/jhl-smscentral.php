5a9faeba71ef4098871c703154eb12b7674fff07
Qiang Hu <qhu@jhldigital.com>
2022-11-26 12:30:41 +0800

    Add plugin jhl-smscentral

---
+++ wp-content/plugins/jhl-smscentral/jhl-smscentral.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * JHL SMS Central
+ *
+ * @copyright Copyright (C) 2018-2022, JHL Digital - hello@jhldigital.com
+ * @license   JHL INTERNAL ONLY
+ *
+ * @wordpress-plugin
+ * Plugin Name: JHL SMS Central
+ * Version:     1.0
+ * Plugin URI:  N/A
+ * Description: SMS Central webhooks, utilities
+ * Author:      JHL Digital
+ * Author URI:  N/A
+ * License:     JHL INTERNAL ONLY
+ *
+ */
+/** ================================================================================= */
+
+require_once 'class-smscentral-webhook.php';
