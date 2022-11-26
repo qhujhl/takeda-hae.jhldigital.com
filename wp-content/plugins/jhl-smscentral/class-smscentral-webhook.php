256f008ecf670a954d041465a6b50d8618425537
Qiang Hu <qhu@jhldigital.com>
2022-11-26 23:20:58 +0800

    Add debug info for SMS central

--- wp-content/plugins/jhl-smscentral/class-smscentral-webhook.php
+++ wp-content/plugins/jhl-smscentral/class-smscentral-webhook.php
@@ -88,6 +88,8 @@ if ( ! class_exists( 'SMSCentral_Webhook' ) ) {
         public function webhook_handler() {
             $input = json_decode( file_get_contents('php://input') );
 
+            error_log( print_r( $_POST, true ) );
+            error_log( print_r( $_REQUEST, true ) );
             error_log( print_r( $input, true ) );
             if( ! $input ) return;
 
